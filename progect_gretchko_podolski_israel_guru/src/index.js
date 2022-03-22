81a1944dc2324ee8ef030fefe524e74716da1f76
Vsevolod Gretchko <gretchkovsvl@gmail.com>
2022-03-22 14:21:13 +0200

    Add footer, header, main, store files

--- progect_gretchko_podolski_israel_guru/src/index.js
+++ progect_gretchko_podolski_israel_guru/src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <BrowserRouter>
+        {/* <Provider store={store}> */}
+        <App />
+        {/* </Provider> */}
+    </BrowserRouter>,
+    document.getElementById('root')
 );
